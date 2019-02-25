ea4c85aa44ef06f48fa46415f70a6085625eef7c
czf826183846 <34672213+czf826183846@users.noreply.github.com>
2019-02-25 10:28:12 +0800

    Create two-dimensional array find.java

---
+++ two-dimensional array find.java
@@ -0,0 +1,17 @@
+//从左下角开始查找
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+        int len = array.length-1;
+        int i = 0;
+        while((len >= 0)&& (i < array[0].length)){
+            if(array[len][i] > target){
+                len--;
+            }else if(array[len][i] < target){
+                i++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
